8f77b678c9530a21b0e2955b613453e763c62be0
BuTn <kimmosc2@163.com>
2020-09-16 22:20:19 +0800

    start chapter03

---
+++ chapter02/code/226/demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned short usx = 53191;
+    unsigned short usx2 = -12345;
+    int x = usx;
+    printf("usx = %u, usx2 = %d, x = %d\n",usx,usx2,x);
+    return 0;
+}
